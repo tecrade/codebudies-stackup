013245230f8c9360abd22d4d3b144f4e9d189606
tecrade <jovinjohn.developer@gmail.com>
2023-11-09 17:30:04 +0530

    modified package.json

---
+++ src/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Productbanner from '../components/productbanner/Productbanner'
+
+function Cart() {
+  return (
+    <div>
+        <Productbanner title="My Cart"/>
+        
+    </div>
+  )
+}
+
+export default Cart
